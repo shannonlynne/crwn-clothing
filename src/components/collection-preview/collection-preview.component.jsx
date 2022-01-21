63c7a8f7d021077373d6361108bbf40491f2a9ff
Shannon Morrell <shannon.morrell@ruralsource.com>
2022-01-21 14:25:08 -0600

    working version - added shop data

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,18 @@
+import { getByTitle } from '@testing-library/react'
+import React from 'react'
+import './collection-preview.styles.scss'
+
+const CollectionPreview = ({title, items}) => (
+    <div>
+        <h1 className='collection-preview'>{title.toUpperCase()}</h1>
+        <div className='preview' >
+            {
+                items.map((item) =>  (
+                    <div key={item.id}>{item.name}</div>
+                ))
+            }
+        </div>
+    </div>
+)
+
+export default CollectionPreview;
