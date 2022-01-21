92db4f17dd9bb2b3ed97216d8194bd03a9c3cca2
Shannon Morrell <shannon.morrell@ruralsource.com>
2022-01-21 16:55:22 -0600

    added images, names, price to the shop screen

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,14 +1,17 @@
-import { getByTitle } from '@testing-library/react'
+
 import React from 'react'
+import CollectionItem from '../collection-item/collection-item.component';
 import './collection-preview.styles.scss'
 
 const CollectionPreview = ({title, items}) => (
-    <div>
-        <h1 className='collection-preview'>{title.toUpperCase()}</h1>
+    <div className='collection-preview'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview' >
             {
-                items.map((item) =>  (
-                    <div key={item.id}>{item.name}</div>
+                items
+                .filter((item, idx) => idx < 4)
+                .map(({id, ...otherItemProps}) => (
+                    <CollectionItem key={id} {...otherItemProps} />
                 ))
             }
         </div>
