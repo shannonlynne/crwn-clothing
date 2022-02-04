2cdf47be7e12f4a06787b80c36e689058c9060e5
Shannon Morrell <shannon.morrell@ruralsource.com>
2022-02-04 12:48:04 -0600

    added login with firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/firestore'
-import 'firebase/compat/auth'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
 
 const config = {
     apiKey: "AIzaSyBfrNu31gZCKoEUGiZH8VTSjErYEkX21hs",
@@ -11,14 +11,38 @@ const config = {
     appId: "1:791993736181:web:7ead55f37f9cf843d0debc"
   };
 
-  firebase.initializeApp(config)
+firebase.initializeApp(config);
 
-  export const auth = firebase.auth()
-  export const firestore = firebase.firestore()
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
 
-  const provider = new firebase.auth.GoogleAuthProvider()
-  provider.setCustomParameters({prompt: 'select_account'})
-  export const signInWithGoogle = () => auth.signInWithPopup(provider)
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-  export default firebase
+  const snapShot = await userRef.get();
 
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
